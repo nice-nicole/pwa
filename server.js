57a6625ff1dce51df0d4dde0d427ced9de2ec179
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-10-21 13:33:38 +0200

    Server of static content

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 let http = require('http')
-let server = http.createServer()
-server.on('request', function(req, res) {
+
+let nodestatic = require('node-static')
+
+let httpServer = http.createServer()
+let fileServer = new nodestatic.Server('./frontend')
+
+httpServer.on('request', function(req, res) {
+    /*
     res.writeHead(200, 'OK', {'Content-type': 'text/plain'})
-    res.write('Success!')
+    res.write('Success! ' + req.url)
     res.end()
+    */
+    fileServer.serve(req, res);
 })
-server.listen(7777)
+
+httpServer.listen(7777)
