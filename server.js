b07c9b343a6dc5df781713f36a725d1b4d604e4b
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-10-22 10:48:21 +0200

    Endpoint /data

--- server.js
+++ server.js
@@ -5,13 +5,29 @@ let nodestatic = require('node-static')
 let httpServer = http.createServer()
 let fileServer = new nodestatic.Server('./frontend')
 
+let data = [
+    { firstName: 'Mariusz', lastName: 'Jarocki', year: 1969 },
+    { firstName: 'John', lastName: 'Doe', year: 2000 }
+]
+
 httpServer.on('request', function(req, res) {
-    /*
-    res.writeHead(200, 'OK', {'Content-type': 'text/plain'})
-    res.write('Success! ' + req.url)
-    res.end()
-    */
-    fileServer.serve(req, res);
+    console.log(req.method, req.url)
+    switch(req.url) {
+
+        case '/data':
+            switch(req.method) {
+                case 'GET':
+                    res.writeHead(200, 'OK', {'Content-type': 'application/json'})
+                    res.write(JSON.stringify(data))
+                    res.end()
+                default:
+                    res.writeHead(405, 'Method not allowed', {'Content-type': 'application/json'})
+                    res.end()
+                }
+
+        default:
+            fileServer.serve(req, res)
+    }
 })
 
 httpServer.listen(7777)
