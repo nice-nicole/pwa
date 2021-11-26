b0fb0c2d959e38e67ea90b2ee6388ececdadb48c
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-11-26 16:08:43 +0100

    Angular Router

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-let app = angular.module('pwa2021', [ 'ngRoute' ])
+let app = angular.module('pwa2021', [ 'ngRoute', 'ngSanitize' ])
 
 // router menu
 app.constant('routes', [
@@ -16,9 +16,35 @@ app.config(['$routeProvider', '$locationProvider', 'routes', function($routeProv
 	$routeProvider.otherwise({ redirectTo: '/' })
 }])
 
-app.controller('Ctrl', [ '$http', function($http) {
+app.controller('Ctrl', [ '$http', '$location', '$scope', 'routes', function($http, $location, $scope, routes) {
     console.log('Ctrl started')
     let ctrl = this
+
+    // menu building
+
+   ctrl.menu = []
+
+   let rebuildMenu = function() {
+       for(var i in routes) {
+           ctrl.menu.push({ route: routes[i].route, title: routes[i].menu })
+       }
+       $location.path('/')
+   }
+
+   // kontrola nad menu zwiniętym i rozwiniętym
+   ctrl.isCollapsed = true
+   $scope.$on('$routeChangeSuccess', function () {
+       ctrl.isCollapsed = true
+   })
+   
+   // sprawdzenie która pozycja menu jest wybrana
+   ctrl.navClass = function(page) {
+       return page === $location.path() ? 'active' : ''
+   }    
+
+   rebuildMenu()
+
+   // end of menu preparation
 
     ctrl.clickedRow = -1
 
