221e7b1a04b1817dbf53a5be361edc0255df4482
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-11-26 18:24:23 +0100

    Move presentations and controllers (except of Transfers) to separate files

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -46,50 +46,6 @@ app.controller('Ctrl', [ '$http', '$location', '$scope', 'routes', function($htt
 
    // end of menu preparation
 
-    ctrl.clickedRow = -1
-
-    ctrl.persons = []
-
-    ctrl.person = {
-        firstName: '',
-        lastName: '',
-        year: 2000
-    }
-
-    ctrl.amount = 0
-    ctrl.from = ctrl.to = null
-
-    ctrl.new = function() {
-        $http.post('/person', ctrl.person).then(function(res) {
-            ctrl.persons = res.data
-        }, function(err) {
-            console.error(err.data)
-        })
-    }
-
-    ctrl.modify = function() {
-        let _id = ctrl.persons[ctrl.clickedRow]._id
-        $http.put('/person?_id=' + _id, ctrl.person).then(function(res) {
-            ctrl.persons = res.data
-        }, function(err) {
-            console.error(err.data)
-        })
-    }
-
-    ctrl.delete = function(_id) {
-        $http.delete('/person?_id=' + _id).then(function(res) {
-            ctrl.persons = res.data
-        }, function(err) {
-            console.error(err.data)
-        })
-    }
-
-    ctrl.copy = function(n) {
-        ctrl.person.firstName = ctrl.persons[n].firstName
-        ctrl.person.lastName = ctrl.persons[n].lastName
-        ctrl.person.year = ctrl.persons[n].year
-        ctrl.clickedRow = n
-    }
 
     ctrl.deposit_on_all = function() {
         $http.post('/balance', { amount: ctrl.amount }).then(function(res) {
@@ -116,15 +72,4 @@ app.controller('Ctrl', [ '$http', '$location', '$scope', 'routes', function($htt
         return ctrl.amount > 0
     }
 
-    ctrl.isPersonDataCorrect = function() {
-        return ctrl.person.firstName && ctrl.person.lastName && ctrl.person.year >= 1500
-    }
-
-    // retrieve persons list on start
-    $http.get('/person').then(function(res) {
-        ctrl.persons = res.data
-        ctrl.from = ctrl.to = ctrl.persons[0]._id
-    }, function(err) {
-        console.error(err.data)
-    })
 }])
--- frontend/js/personsCtrl.js
+++ frontend/js/personsCtrl.js
@@ -1,3 +1,62 @@
-app.controller('PersonsCtrl', [ function() {
+app.controller('PersonsCtrl', [ '$http', function($http) {
     console.log('PersonsCtrl started')
+    let ctrl = this
+
+    ctrl.clickedRow = -1
+
+    ctrl.persons = []
+
+    ctrl.person = {
+        firstName: '',
+        lastName: '',
+        year: 2000
+    }
+
+    ctrl.amount = 0
+    ctrl.from = ctrl.to = null
+
+    ctrl.new = function() {
+        $http.post('/person', ctrl.person).then(function(res) {
+            ctrl.persons = res.data
+        }, function(err) {
+            console.error(err.data)
+        })
+    }
+
+    ctrl.modify = function() {
+        let _id = ctrl.persons[ctrl.clickedRow]._id
+        $http.put('/person?_id=' + _id, ctrl.person).then(function(res) {
+            ctrl.persons = res.data
+        }, function(err) {
+            console.error(err.data)
+        })
+    }
+
+    ctrl.delete = function(_id) {
+        $http.delete('/person?_id=' + _id).then(function(res) {
+            ctrl.persons = res.data
+        }, function(err) {
+            console.error(err.data)
+        })
+    }
+
+    ctrl.copy = function(n) {
+        ctrl.person.firstName = ctrl.persons[n].firstName
+        ctrl.person.lastName = ctrl.persons[n].lastName
+        ctrl.person.year = ctrl.persons[n].year
+        ctrl.clickedRow = n
+    }
+
+    ctrl.isPersonDataCorrect = function() {
+        return ctrl.person.firstName && ctrl.person.lastName && ctrl.person.year >= 1500
+    }
+
+    // retrieve persons list on start
+    $http.get('/person').then(function(res) {
+        ctrl.persons = res.data
+        ctrl.from = ctrl.to = ctrl.persons[0]._id
+    }, function(err) {
+        console.error(err.data)
+    })
+
 }])
