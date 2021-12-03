b7cc96c0a61b996144a3e9f7e245196feab6e67e
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-12-03 12:34:17 +0100

    Deposit on all accounts. "when" in transaction object

--- transfer.js
+++ transfer.js
@@ -15,15 +15,33 @@ const transfer = module.exports = {
     },
 
     handle: function(env) {
+        let now = Date.now()
         switch(env.req.method) {
             case 'POST':
                 // deposit/withdraw the amount on/from all accounts
-                lib.sendError(env.res, 405, 'Method not yet implemented (homework')
+                if(transfer.validateAmount(env.payload.amount)) {
+                    let deposits = []
+                    db.persons.find().toArray(function(err, persons) {
+                        persons.forEach(function(person) {
+                            deposits.push({ person_id: person._id, amount: env.payload.amount, when: now })
+                        })
+                        // create an object in transactions for each person
+                        db.transactions.insertMany(deposits, function(err, result) {
+                            if(!err) {
+                                lib.sendJson(env.res, result.insertedIds)
+                            } else {
+                                lib.sendError(env.res, 400, 'Inserting transactions failed')
+                            }
+                        })
+                    })
+                } else {
+                    lib.sendError(env.res, 400, 'Amount is invalid')
+                }
                 break
             case 'PUT':
                 // transfer the amount from one account to another
-                let transactionIn = { person_id: env.payload.to, amount: env.payload.amount }
-                let transactionOut = { person_id: env.payload.from, amount: -env.payload.amount }
+                let transactionIn = { person_id: env.payload.to, amount: env.payload.amount, when: now }
+                let transactionOut = { person_id: env.payload.from, amount: -env.payload.amount, when: now }
                 if(transfer.validateTransaction(transactionIn) && transfer.validateTransaction(transactionOut)) {
                     // create two objects in transactions
                     db.transactions.insertMany([ transactionIn, transactionOut ], function(err, result) {
