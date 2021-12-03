b601a21a32877f67b423635836358aa46500d271
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-12-03 19:10:26 +0100

    History for selected user

--- frontend/js/historyCtrl.js
+++ frontend/js/historyCtrl.js
@@ -3,12 +3,27 @@ app.controller('HistoryCtrl', [ '$http', function($http) {
     let ctrl = this
 
     ctrl.transactions = []
+    ctrl.person_id = null
 
     // retrieve persons list on start
-    $http.get('/history?_id=619763a2522e0e4bbc5c9412').then(function(res) {
-        ctrl.transactions = res.data
+    $http.get('/person').then(function(res) {
+        ctrl.persons = res.data
+        ctrl.person_id = ctrl.persons[0]._id
+        ctrl.refreshHistory()
     }, function(err) {
         console.error(err.data)
     })
+    
+    ctrl.refreshHistory = function() {
+        $http.get('/history?_id=' + ctrl.person_id).then(function(res) {
+            ctrl.transactions = res.data
+            ctrl.transactions.forEach(function(transaction) {
+                let date = new Date(transaction.when)
+                transaction.when = date.toLocaleDateString() + ' ' + date.toLocaleTimeString()
+            })
+        }, function(err) {
+            console.error(err.data)
+        })
+    }
 
 }])
