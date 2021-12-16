da8ef8377f4bb5802390b985af9c142535b140b0
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-12-16 13:43:08 +0100

    Simple security rules

--- security.js
+++ security.js
@@ -1,6 +1,21 @@
+let lib = require('./lib')
+
 const security = module.exports = {
 
+    permissions: [
+        { pattern: '^GET /favicon.ico', result: 'forbidden' },
+        { pattern: '^GET /person', role: 'admin', result: null },
+        { pattern: '^GET /person', role: '*', result: 'only for admin' },
+    ],
+
     isForbidden: function(env) {
+        let reqStr = env.req.method + ' ' + env.urlParsed.pathname
+        let role = lib.sessions[env.session].role
+        for(let i in security.permissions) {
+            if(new RegExp(security.permissions[i].pattern).test(reqStr) && (security.permissions[i].role == '*' || security.permissions[i].role == role)) {
+                return security.permissions[i].result
+            }
+        }
         return null
     }
 
